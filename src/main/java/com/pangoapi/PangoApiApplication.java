5347626c527f50319d332f8c275f994a780f5d90
hjjae2 <hjjae2@gmail.com>
2020-08-26 22:35:35 +0900

    #4 프로젝트 생성, gitignore 추가

---
+++ src/main/java/com/pangoapi/PangoApiApplication.java
@@ -0,0 +1,13 @@
+package com.pangoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pangoapi/PangoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pangoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
