12fe951036daa5d32ce43901ff6dac7c77dbafa3
Lee.hyunjae <35790290+hjjae2@users.noreply.github.com>
2020-09-20 19:18:46 +0900

    [개발] Advertisement, AdvertisementType - Test 코드 개발 (#12)
    [개발] Advertisement, AdvertisementType - Test 코드 개발 (#12)
    
    * #10 Test 코드 작성: AdvertisementApiControllerTest, AdvertisementServiceTest, AdvertisementTypeRepositoryTest
    * #10 AdvertisementTypeRepositoryTest 메소드명 수정
    
    [주요 작업 내용]
    * Advertisement API Controller 테스트 코드 작성
    * Advertisement Service 테스트 코드 작성
    * AdvertisementType Repository 테스트 코드 작성

---
+++ src/test/java/com/pangoapi/controller/advertisement/AdvertisementApiControllerTest.java
@@ -0,0 +1,114 @@
+package com.pangoapi.controller.advertisement;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pangoapi.dto.advertisement.CreateDtoAdvertisement;
+import com.pangoapi.dto.advertisement.RetrieveDtoAdvertisement;
+import com.pangoapi.dto.advertisement.UpdateDtoAdvertisement;
+import com.pangoapi.service.advertisement.AdvertisementService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * [References]
+ * 1. https://www.baeldung.com/spring-boot-testing
+ * */
+
+@WebMvcTest(value = AdvertisementApiController.class)
+class AdvertisementApiControllerTest {
+
+    @Autowired MockMvc mockMvc;
+    @Autowired ObjectMapper objectMapper;
+
+    @MockBean AdvertisementService advertisementService;
+
+    private CreateDtoAdvertisement createDtoAdvertisement;
+    private RetrieveDtoAdvertisement retrieveDtoAdvertisement;
+    private UpdateDtoAdvertisement updateDtoAdvertisement;
+
+    @BeforeEach
+    void setUp() {
+        createDtoAdvertisement = new CreateDtoAdvertisement("title", "userEmail", "briefDescription", "detailDescription", "1", "imagePath", "siteUrl", 1L, 1L);
+        retrieveDtoAdvertisement = new RetrieveDtoAdvertisement(1L, "user@email.com", "title", "briefDescription", "detailDescription", "1", 300L, 250L, "imagePath", "siteUrl", 1L, 1L, true);
+        updateDtoAdvertisement = new UpdateDtoAdvertisement(1L, "user@email.com", "title", "briefDescription", "detailDescription", "1", "imagePath", "siteUrl",1L, 1L, true);
+
+        Mockito.when(advertisementService.createOneAdvertisement(any(CreateDtoAdvertisement.class))).thenReturn(1L);
+        Mockito.when(advertisementService.retrieveOneAdvertisement(any(Long.class))).thenReturn(retrieveDtoAdvertisement);
+        Mockito.when(advertisementService.retrieveAllAdvertisement()).thenReturn(new ArrayList<>(Arrays.asList(retrieveDtoAdvertisement)));
+        Mockito.when(advertisementService.updateOneAdvertisement(any(Long.class), any(UpdateDtoAdvertisement.class))).thenReturn(1L);
+    }
+
+    @Test
+    public void advertisement_한건_생성() throws Exception {
+        String content = objectMapper.writeValueAsString(createDtoAdvertisement);
+
+        mockMvc.perform(post("/api/v1/advertisements")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .content(content))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void advertisement_한건_조회() throws Exception {
+        MockHttpServletResponse mockHttpServletResponse = mockMvc.perform(get("/api/v1/advertisements/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isMap())
+                .andDo(print())
+                .andReturn().getResponse();
+
+        System.out.println(mockHttpServletResponse.getContentAsString());
+    }
+
+    @Test
+    public void advertisement_전체_조회() throws Exception {
+        MockHttpServletResponse mockHttpServletResponse = mockMvc.perform(get("/api/v1/advertisements"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andDo(print())
+                .andReturn().getResponse();
+
+        System.out.println(mockHttpServletResponse.getContentAsString());
+    }
+
+    @Test
+    public void advertisement_한건_업데이트() throws Exception {
+        String content = objectMapper.writeValueAsString(updateDtoAdvertisement);
+
+        mockMvc.perform(put("/api/v1/advertisements/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void advertisement_한건_삭제() throws Exception {
+        MockHttpServletResponse mockHttpServletResponse = mockMvc.perform(delete("/api/v1/advertisements/1"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn().getResponse();
+
+        System.out.println(mockHttpServletResponse.getContentAsString());
+    }
+}
