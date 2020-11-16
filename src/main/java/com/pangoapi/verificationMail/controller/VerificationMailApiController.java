fe968e5eddd51f2d14f318b2126d8674d86c59f1
hjjae2 <hjjae2@gmail.com>
2020-11-16 22:54:04 +0900

    #34 Controller, Service 코드 수정

--- src/main/java/com/pangoapi/verificationMail/controller/VerificationMailApiController.java
+++ src/main/java/com/pangoapi/verificationMail/controller/VerificationMailApiController.java
@@ -1,13 +1,11 @@
 package com.pangoapi.verificationMail.controller;
 
 import com.pangoapi._common.dto.ApiResponse;
-import com.pangoapi._common.utility.MailHandler;
 import com.pangoapi.verificationMail.dto.RequestDtoVerificationMail;
 import com.pangoapi.verificationMail.dto.ResponseDtoVerificationMail;
 import com.pangoapi.verificationMail.service.VerificationMailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class VerificationMailApiController {
 
     private final VerificationMailService verificationMailService;
-    private final JavaMailSender javaMailSender;
 
     @PostMapping("/api/v1/verification-mails")
     public ApiResponse sendVerificationEmail(@RequestBody RequestDtoVerificationMail requestDtoVerificationMail) throws Exception {
-        MailHandler mailHandler = new MailHandler(javaMailSender);
-        ResponseDtoVerificationMail responseDtoVerificationMail = new ResponseDtoVerificationMail();
-
-        responseDtoVerificationMail = verificationMailService.sendVerificationEmail(mailHandler, responseDtoVerificationMail, requestDtoVerificationMail);
+        ResponseDtoVerificationMail responseDtoVerificationMail = verificationMailService.sendVerificationEmail(requestDtoVerificationMail);
 
         return new ApiResponse(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(), responseDtoVerificationMail);
     }
--- src/main/java/com/pangoapi/verificationMail/service/VerificationMailService.java
+++ src/main/java/com/pangoapi/verificationMail/service/VerificationMailService.java
@@ -7,6 +7,7 @@ import com.pangoapi.verificationMail.dto.ResponseDtoVerificationMail;
 import com.pangoapi.verificationMail.repository.VerificationMailRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,19 +22,31 @@ public class VerificationMailService {
 
     private final Environment environment;
     private final VerificationMailRepository verificationMailRepository;
+    private final JavaMailSender javaMailSender;
     private Long maximumNumberOfRequests = 5L;
+
+    protected ResponseDtoVerificationMail makeResponseDtoVerificationMail() {
+        return new ResponseDtoVerificationMail();
+    }
+
+    protected MailHandler makeMailHandler(JavaMailSender javaMailSender) throws Exception {
+        return new MailHandler(javaMailSender);
+    }
 
     /**
      * CREATE
      * */
     @Transactional
-    public ResponseDtoVerificationMail sendVerificationEmail(MailHandler mailHandler, ResponseDtoVerificationMail responseDtoVerificationMail, RequestDtoVerificationMail requestDtoVerificationMail) throws Exception {
+    public ResponseDtoVerificationMail sendVerificationEmail(RequestDtoVerificationMail requestDtoVerificationMail) throws Exception {
         if(!VerificationMail.checkEmailFormat(requestDtoVerificationMail.getReceiver())) {
             throw new IllegalArgumentException("적절하지 않은 이메일 형식 입니다. (Please check the email)");
         }
         if(verificationMailRepository.countByToEmailAndSentDate(requestDtoVerificationMail.getReceiver(), LocalDate.now()) >= maximumNumberOfRequests) {
             throw new LimitExceededException("일일 API 요청 횟수를 초과했습니다. (Exceeded 5 API requests)");
         }
+
+        ResponseDtoVerificationMail responseDtoVerificationMail = this.makeResponseDtoVerificationMail();
+        MailHandler mailHandler = this.makeMailHandler(javaMailSender);
 
         String toEmail = requestDtoVerificationMail.getReceiver();
         String fromEmail = environment.getProperty("application.mail.from");
