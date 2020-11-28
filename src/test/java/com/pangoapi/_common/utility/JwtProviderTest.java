36b675b3ddd5e4cb8e22a572075f4e610ab953ae
hjjae2 <hjjae2@gmail.com>
2020-11-29 01:28:46 +0900

    #55 Add a test code of JwtProvider.class

---
+++ src/test/java/com/pangoapi/_common/utility/JwtProviderTest.java
@@ -0,0 +1,47 @@
+package com.pangoapi._common.utility;
+
+import io.jsonwebtoken.Claims;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(
+        classes = {
+                JwtProvider.class,
+        }
+)
+class JwtProviderTest {
+
+    @Autowired JwtProvider jwtProvider;
+
+    private long expirationAge = 60 * 60 * 1000;
+    private String role = "ROLE_USER";
+    private String audience = "pigs95team";
+    private String subject = "subject";
+
+    @Test
+    public void When_call_generateToken_Then_return_JWT() {
+        // Given // When
+        String jwt = jwtProvider.generateToken(subject, audience, role);
+
+        // Then
+        assertThat(jwt.getClass()).isEqualTo(String.class);
+    }
+
+    @Test
+    public void When_call_getPayloadFromToken_Then_return_payload() {
+        // Given
+        String jwt = jwtProvider.generateToken(subject, audience, role);
+
+        // When
+        Claims parsedJwt = jwtProvider.getPayloadFromToken(jwt);
+
+        // Then
+        assertThat(parsedJwt.get("role")).isEqualTo(role);
+        assertThat(parsedJwt.getSubject()).isEqualTo(subject);
+        assertThat(parsedJwt.getAudience()).isEqualTo(audience);
+        assertThat(parsedJwt.getExpiration().getTime() - parsedJwt.getIssuedAt().getTime()).isEqualTo(expirationAge);
+    }
+}
