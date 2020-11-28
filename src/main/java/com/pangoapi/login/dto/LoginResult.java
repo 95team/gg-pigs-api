c501f4555ba52e94b032e09a55d1ccdf1df83162
hjjae2 <hjjae2@gmail.com>
2020-11-29 01:27:56 +0900

    #55 Remove 'Builder' annotation from LoginResult.class

--- src/main/java/com/pangoapi/login/dto/LoginResult.java
+++ src/main/java/com/pangoapi/login/dto/LoginResult.java
@@ -1,13 +1,11 @@
 package com.pangoapi.login.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Getter
 public class LoginResult {
 
