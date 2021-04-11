cd11825a642ff12d91810467eef7c9c5eb745969
hjjae2 <hjjae2@gmail.com>
2021-04-11 14:01:18 +0900

    #117 HistoryLogService -> HistoryLogService, HistoryLogServiceImpl

--- src/test/java/com/gg_pigs/historyLog/service/HistoryLogServiceTest.java
+++ src/test/java/com/gg_pigs/historyLog/service/HistoryLogServiceTest.java
@@ -18,7 +18,7 @@ import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest(
         classes = {
-                HistoryLogService.class,
+                HistoryLogServiceImpl.class,
         }
 )
 class HistoryLogServiceTest {
