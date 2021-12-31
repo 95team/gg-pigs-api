faa9c9df2969c73e9bb52f6bbb51c8ae34c934e0
hjjae2 <hjjae2@gmail.com>
2021-12-31 13:23:14 +0900

    Cookie domain 명시

--- src/main/java/com/gg_pigs/_common/utility/CookieProvider.java
+++ src/main/java/com/gg_pigs/_common/utility/CookieProvider.java
@@ -18,26 +18,28 @@ public class CookieProvider {
     private final int expiry = 3600;
     private final boolean isHttpOnly = true;
     private final boolean isSecure = false;
+    private final String domain = "gg-pigs.com";
 
     @Value("${application.cookie.login-cookie-name}")
     private String loginCookieName;
 
-    public Cookie generateCookie(String name, String value) {
-        return this.generateCookie(name, value, this.uri, this.expiry, this.isHttpOnly, this.isSecure);
+    public Cookie generateLoginCookie(String value) {
+        return this.generateCookie(this.loginCookieName, value);
     }
 
-    private Cookie generateCookie(String name, String value, String uri, int expiry, boolean httpOnly, boolean secure) {
+    public Cookie generateCookie(String name, String value) {
+        return this.generateCookie(name, value, this.uri, this.expiry, this.isHttpOnly, this.isSecure, this.domain);
+    }
+
+    private Cookie generateCookie(String name, String value, String uri, int expiry, boolean httpOnly, boolean secure, String domain) {
         Cookie cookie = new Cookie(name, value);
         cookie.setPath(uri);
         cookie.setMaxAge(expiry);
         cookie.setHttpOnly(httpOnly);
         cookie.setSecure(secure);
+        cookie.setDomain(domain);
 
         return cookie;
-    }
-
-    public Cookie generateLoginCookie(String value) {
-        return this.generateCookie(this.loginCookieName, value);
     }
 
     public Cookie destroyCookie(String name, String value) {
@@ -45,7 +47,7 @@ public class CookieProvider {
          * [Note]
          * 1. MaxAge 를 '0' 으로 설정하여 쿠키를 삭제합니다.
          * */
-        return this.generateCookie(name, value, this.uri, 0, this.isHttpOnly, this.isSecure);
+        return this.generateCookie(name, value, this.uri, 0, this.isHttpOnly, this.isSecure, this.domain);
     }
 
     public Cookie destroyLoginCookie() {
